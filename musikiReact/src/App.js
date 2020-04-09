0c768e69eee7d441bc0ca5f7d657bbfe82d876e1
Gabriel Mello <gabrielbramello@gmail.com>
2020-04-09 17:16:30 -0300

    remoção node_modules, att README

---
+++ musikiReact/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from './components/HelloWorld';
+import Dashboard from './Dashboard';
+import './App.css';
+
+function App() {
+  return (
+    <Dashboard/>
+  );
+}
+
+export default App;
---
+++ musikiReact/src/components/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Menu from '@material-ui/core/Menu';
+
+
+class Menu extends React.Component {
+    render() {
+      
+  
+    }
+  }
+export default HelloWorld;
