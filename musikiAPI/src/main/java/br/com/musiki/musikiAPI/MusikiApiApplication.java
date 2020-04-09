eaa0559f5086f475d5db0ff5a48222a636580561
Gabriel Mello <gabrielbramello@gmail.com>
2020-04-09 19:35:38 -0300

    commit inicial musikiAPI

---
+++ musikiAPI/src/main/java/br/com/musiki/musikiAPI/MusikiApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.musiki.musikiAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusikiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusikiApiApplication.class, args);
+	}
+
+}
---
+++ musikiAPI/src/test/java/br/com/musiki/musikiAPI/MusikiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.musiki.musikiAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusikiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
