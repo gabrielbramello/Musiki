f9df09c878daf89a29c1e85b0a1d8b5a278b8341
Gabriel Mello <gabrielbramello@gmail.com>
2021-04-09 18:15:34 -0300

    Implementação classe de autorização para acesso api spotify

---
+++ musikiAPI/src/main/java/br/com/musiki/musikiAPI/services/spotify/authorization/ClientCredentialAuth.java
@@ -0,0 +1,64 @@
+package br.com.musiki.musikiAPI.services.spotify.authorization;
+
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
+import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+
+import br.com.musiki.musikiAPI.configuration.Properties;
+
+import org.apache.hc.core5.http.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
+public class ClientCredentialAuth {
+
+	@Autowired
+	public static Properties properties;
+	
+	private static final String clientId = properties.getSpotifyApi().getClientId();
+	private static final String clientSecret = properties.getSpotifyApi().getClientSecret();
+
+	private static final SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId(clientId)
+			.setClientSecret(clientSecret).build();
+	private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
+
+	public static void clientCredentials_Sync() {
+		try {
+			final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+
+			// Set access token for further "spotifyApi" object usage
+			spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+			System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+		} catch (IOException | SpotifyWebApiException | ParseException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+
+	public static void clientCredentials_Async() {
+		try {
+			final CompletableFuture<ClientCredentials> clientCredentialsFuture = clientCredentialsRequest
+					.executeAsync();
+
+			// Thread free to do other tasks...
+
+			// Example Only. Never block in production code.
+			final ClientCredentials clientCredentials = clientCredentialsFuture.join();
+
+			// Set access token for further "spotifyApi" object usage
+			spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+			System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+		} catch (CompletionException e) {
+			System.out.println("Error: " + e.getCause().getMessage());
+		} catch (CancellationException e) {
+			System.out.println("Async operation cancelled.");
+		}
+	}
+
+}
