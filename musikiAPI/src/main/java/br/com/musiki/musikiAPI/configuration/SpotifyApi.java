49ab3a28a207c4f004437e325292e231e2d52349
Gabriel Mello <gabrielbramello@gmail.com>
2021-04-09 18:14:56 -0300

    Classes de configuração para pegar clientid e secret de application.prop

---
+++ musikiAPI/src/main/java/br/com/musiki/musikiAPI/configuration/SpotifyApi.java
@@ -0,0 +1,21 @@
+package br.com.musiki.musikiAPI.configuration;
+
+public class SpotifyApi {
+	public String clientId;
+	public String clientSecret;
+	
+	public String getClientId() {
+		return clientId;
+	}
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	public String getClientSecret() {
+		return clientSecret;
+	}
+	public void setClientSecret(String clientSecret) {
+		this.clientSecret = clientSecret;
+	}
+	
+	
+}
