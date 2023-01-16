c9cc0f6a2659221d16cde6df06078aa791997222
Leonardo Teixeira <leonardo.teixeira@osm.com.br>
2023-01-16 13:35:26 -0300

    Adição componente Menubar e logo

--- musikiprimefaces/src/components/menu/Menu.js
+++ musikiprimefaces/src/components/menu/Menu.js
@@ -2,24 +2,43 @@ import React, { useState } from 'react';
 import "primereact/resources/themes/mdc-light-deeppurple/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
-import { TabMenu } from 'primereact/tabmenu';
 import { Link } from 'react-router-dom';
+import { Menubar } from 'primereact/menubar';
+import logo from '../../images/logo-color.png';
+
 
 export default function Menu() {
 
-    const [activeIndex, setActiveIndex] = useState(5);
-
     const items = [
-        { label: 'Pagina Inicial', icon: 'pi pi-fw pi-home', },
-        { label: 'Artistas', icon: 'pi pi-fw pi-at', onClick: ()=>{return <Link to='/artist/74XFHRwlV6OrjEM0A2NCMF'/>} },
-        { label: 'Albums', icon: 'pi pi-fw pi-book' },
-        { label: 'Musicas', icon: 'pi pi-fw pi-volume-up' },
-        { label: 'Pagina do Usuario', icon: 'pi pi-fw pi-user' },
-        { label: 'Configurações', icon: 'pi pi-fw pi-cog' }
-
+        {
+            label: 'Página Inicial',
+            icon: 'pi pi-fw pi-home'
+        },
+        {
+            label: 'Artistas',
+            icon: 'pi pi-fw pi-at'
+        },
+        {
+            label: 'Albums',
+            icon: 'pi pi-fw pi-book'
+        },
+        {
+            label: 'Musicas',
+            icon: 'pi pi-fw pi-volume-up'
+        },
+        {
+            label: 'Página do Usuário',
+            icon: 'pi pi-fw pi-user'
+        },
+        {
+            label: 'Configurações',
+            icon: 'pi pi-fw pi-cog'
+        }
     ];
 
+    const start = <img alt="logo" height= "40px" width= "50px" src={logo}></img>
+
     return (
-            <TabMenu model={items} activeIndex={activeIndex} onTabChange={(e) => { e.value.onClick()}} />
+                <Menubar style={{ height: "50px"}} model={items} start={start} />
     )
 }
