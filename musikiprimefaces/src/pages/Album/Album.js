99ac6fde932953d247570110ee486f3317280c02
Gabriel Mello <gabrielbramello@gmail.com>
2023-01-26 17:13:48 -0300

    Ajuste carregamento página

--- musikiprimefaces/src/pages/Album/Album.js
+++ musikiprimefaces/src/pages/Album/Album.js
@@ -16,46 +16,46 @@ const Album = () => {
 	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-	  // Set the loading flag to true
-	  setIsLoading(true);
+		// Set the loading flag to true
+		setIsLoading(true);
 
-	  // Use the axios.get() method to make a GET request to the API endpoint
-	  axios.get('/spotify/album/' + albumId)
-	  	.then(response => {
-		  // Update the data and loading flag state
-		  setData(response.data);
-		  setIsLoading(false);
-		})
-		.catch(error => {
-		  // Handle any errors that occurred during the request
-		  console.error(error);
-		  // Set the loading flag to false
-		  setIsLoading(false);
-		});
+		// Use the axios.get() method to make a GET request to the API endpoint
+		axios.get('/spotify/album/' + albumId)
+			.then(response => {
+				// Update the data and loading flag state
+				setData(response.data);
+			})
+			.catch(error => {
+				// Handle any errors that occurred during the request
+				console.error(error);
+				// Set the loading flag to false
+				setIsLoading(false);
+			});
+		setIsLoading(false);
 	}, []);
 
 	if (isLoading) {
-	  return <p>Api do Spotify Carregando...</p>;
+		return <p>Api do Spotify Carregando...</p>;
 	}
 
 	function convertMilliseconds(data) {
 		var minutes = Math.floor(data / 60000);
 		var seconds = ((data % 60000) / 1000).toFixed(0);
 		return "" + minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-	  }
+	}
 
-	  function capitalizeEachWord(sentence) {
+	function capitalizeEachWord(sentence) {
 		return sentence.split(' ')
-		  .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-		  .join(' ');
-	  }
+			.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+			.join(' ');
+	}
 
-	  function changeDateFormat(dateString) {
+	function changeDateFormat(dateString) {
 		// Use o método split() para dividir a string da data em ano, mês e dia
 		const dateArr = dateString.split("-");
 		// Verifica se o formato é válido (yyyy-mm-dd)
 		if (dateArr.length !== 3) {
-		  return "Formato de data inválido!";
+			return "Formato de data inválido!";
 		}
 		// Adiciona zeros a esquerda para os dias e meses com menos de 2 dígitos
 		const day = dateArr[2].padStart(2, "0");
@@ -64,33 +64,39 @@ const Album = () => {
 		// Junta as partes da data no formato correto (dd-mm-yyyy)
 		const newDateFormat = `${day}-${month}-${year}`;
 		return newDateFormat;
-	  }
+	}
 
 	console.log(data)
 
 	return (
 		<div>
-            <Header />
-			<div style={{ background: 'linear-gradient(90deg, rgba(91,22,176,1) 22%, rgba(34,198,216,1) 66%)', minHeight: '90vh', display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', flexWrap: 'wrap' }}>
-				<div style={{ margin: '50px' }}>
-					<img alt="alternatetext" src={data.images && data.images[1].url} style={{ minHeight: '320px', minWidth: '320px' }}></img>
-				</div>
-				<div style={{ textAlign: 'center',display: 'flex', flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap', margin: '50px' }}>
-					<div>
-						<SimpleCard title="Nome do(a) Artista:" width="12rem" bottom="2em" content={data.artists && data.artists[0].name} isRating={false}></SimpleCard>
-						<SimpleCard title="Nome do Album:" width="12rem" bottom="2em" content={data.name} isRating={false}></SimpleCard>
-						<SimpleCard title="Numero de Faixas:" width="12rem" bottom="2em" content={data.tracks && data.tracks.total} isRating={false}></SimpleCard>
+			<Header />
+			{isLoading ?
+				<p>Api do Spotify Carregando...</p> 
+				:
+				<div style={{ background: 'linear-gradient(90deg, rgba(91,22,176,1) 22%, rgba(34,198,216,1) 66%)', minHeight: '90vh', display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', flexWrap: 'wrap' }}>
+					<div style={{ margin: '50px' }}>
+						<img alt="alternatetext" src={data.images && data.images[1].url} style={{ minHeight: '320px', minWidth: '320px' }}></img>
+					</div>
+					<div style={{ textAlign: 'center', display: 'flex', flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap', margin: '50px' }}>
+						<div>
+							<SimpleCard title="Nome do(a) Artista:" width="12rem" bottom="2em" content={data.artists && data.artists[0].name} isRating={false}></SimpleCard>
+							<SimpleCard title="Nome do Album:" width="12rem" bottom="2em" content={data.name} isRating={false}></SimpleCard>
+							<SimpleCard title="Numero de Faixas:" width="12rem" bottom="2em" content={data.tracks && data.tracks.total} isRating={false}></SimpleCard>
+						</div>
+						<div>
+							<SimpleCard title="Gravadora:" width="12rem" bottom="2em" content={data.label} isRating={false}></SimpleCard>
+							<SimpleCard title="Popularidade:" width="12rem" bottom="2em" content={data.popularity} isRating={true}></SimpleCard>
+							<SimpleCard title="Data de Lançamento:" width="12rem" bottom="2em" content={changeDateFormat(data.releaseDate)} isRating={false}></SimpleCard>
+						</div>
 					</div>
 					<div>
-						<SimpleCard title="Gravadora:" width="12rem" bottom="2em" content={data.label} isRating={false}></SimpleCard>
-						<SimpleCard title="Popularidade:" width="12rem" bottom="2em" content={data.popularity} isRating={true}></SimpleCard>
-						<SimpleCard title="Data de Lançamento:" width="12rem" bottom="2em" content={changeDateFormat(data.releaseDate)} isRating={false}></SimpleCard>
+						<CardWithList title="Detalhes do Disco:" width="30rem" bottom="2em" content={data.tracks && data.tracks.items} />
 					</div>
 				</div>
-				<div>
-					<CardWithList title="Detalhes do Disco:" width="30rem" bottom="2em" content={data.tracks && data.tracks.items} />
-				</div>
-			</div>
+			}
+
+
 
 		</div>
 	)
