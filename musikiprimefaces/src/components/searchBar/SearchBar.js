ae8401b308eeece14ad11bff13607cba17694871
Leonardo Teixeira <leonardo.teixeira@osm.com.br>
2024-04-25 15:15:21 -0300

    Melhoria no retorno dos resultados do componente SearchBar

--- musikiprimefaces/src/components/searchBar/SearchBar.js
+++ musikiprimefaces/src/components/searchBar/SearchBar.js
@@ -4,41 +4,25 @@ import 'primeicons/primeicons.css';
 import axios from "../../apis/api";
 import { Link } from "react-router-dom";
 
-
 function SearchBar({ placeholder }) {
-
   const [filteredData, setFilteredData] = useState([]);
   const [wordEntered, setWordEntered] = useState("");
-  const [data, setData] = useState([]);
 
   useEffect(() => {
-    let isMounted = true;
-    axios.get('/spotify/search/' + wordEntered)
-      .then(response => {
-        if (isMounted) {
-          setData(response.data);
-        }
-      })
-      .catch(error => console.log(error.message));
-
-    return () => {
-      isMounted = false;
-    };
+    if (wordEntered) {
+      axios.get('/spotify/search/' + wordEntered)
+        .then(response => {
+          setFilteredData(response.data);
+        })
+        .catch(error => console.log(error.message));
+    } else {
+      setFilteredData([]);
+    }
   }, [wordEntered]);
 
   const handleFilter = (event) => {
     const searchWord = event.target.value.toLowerCase();
     setWordEntered(searchWord);
-
-  const newFilter = data.filter((value) => {
-      return value.name.toLowerCase().includes(searchWord);
-    });
-
-    if (searchWord === "") {
-      setFilteredData([]);
-    } else {
-      setFilteredData(newFilter);
-    }
   };
 
   const clearInput = () => {
@@ -47,7 +31,6 @@ function SearchBar({ placeholder }) {
   };
 
   return (
-
     <div className="search">
       <div className="searchInputs">
         <input
@@ -59,20 +42,18 @@ function SearchBar({ placeholder }) {
         <div className="searchIcon">
           {filteredData.length === 0 ? (
             <i className="pi pi-search" style={{'fontSize': '2em'}}></i>
-           ) : (
-            <i className="pi pi-times" style={{'fontSize': '2em'}} id="clearBtn" onClick={clearInput} ></i>
+          ) : (
+            <i className="pi pi-times" style={{'fontSize': '2em'}} id="clearBtn" onClick={clearInput}></i>
           )}
         </div>
       </div>
       {filteredData.length !== 0 && (
         <div className="dataResult">
-          {filteredData.slice(0, 15).map((value, key) => {
-            return (
-              <Link className="dataItem" key={key} to= { `${value.type}/${value.id}`}>
-                <p>{`${value.name} - (${value.type})`}</p>
-              </Link>
-            );
-          })}
+          {filteredData.slice(0, 15).map((value, key) => (
+            <Link className="dataItem" key={key} to={`${value.type}/${value.id}`}>
+              <p>{`${value.name} - (${value.type})`}</p>
+            </Link>
+          ))}
         </div>
       )}
     </div>
