e45ce1f40755ad25b5c2bb809252d0d500234773
Leonardo Teixeira <leonardo.teixeira@osm.com.br>
2024-06-27 12:24:16 -0300

    Ajuste url base api

--- musikiprimefaces/src/apis/api.js
+++ musikiprimefaces/src/apis/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = "http://localhost:8082/api"
+const BASE_URL = "https://musiki-api.onrender.com:8082/api"
 
 export default axios.create({
     baseURL:BASE_URL,
