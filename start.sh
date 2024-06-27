d105fd9ca5965d1748bcd626c1dd5839296c3500
Leonardo Teixeira <leonardo.teixeira@osm.com.br>
2024-06-26 22:45:58 -0300

    Ajustes nos arquivos docker

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 docker-compose down
 
+git pull
+
 docker build -t musiki:latest ./musikiAPI
 
 docker build -t musiki-primefaces:latest ./musikiprimefaces
 
 docker-compose up --build --force-recreate --remove-orphans
+
+docker-compose logs --tail all -f spring-app
