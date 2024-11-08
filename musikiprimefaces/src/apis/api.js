75adf7cf0a5a581466efa2876f4582ef7f112463
Leonardo <leonardo.teixeira@osm.com.br>
2024-11-08 17:07:31 -0300

    Ajuste porta url base react

--- musikiprimefaces/src/apis/api.js
+++ musikiprimefaces/src/apis/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-const BASE_URL = "https://musiki-api.onrender.com:8082/api"
+//const BASE_URL = "https://musiki-api.onrender.com:8082/api"
+
+const BASE_URL = "http://localhost:8080/api"
 
 export default axios.create({
     baseURL:BASE_URL,
