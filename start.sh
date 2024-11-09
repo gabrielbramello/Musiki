ac3a68576de3483acb54f462ee7c4fb31567ca8f
Leonardo <leonardo.teixeira@osm.com.br>
2024-11-09 01:52:30 -0300

    Refinamento script start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,22 @@
+# Parando e removendo containers existentes
 echo "Parando e removendo containers existentes..."
 docker-compose down
 
+# Atualizando o código do repositório
 echo "Atualizando o código do repositório..."
 git pull
 
+# Construindo a imagem do Docker para a aplicação
 echo "Construindo a imagem do Docker para a aplicação..."
 docker-compose build --no-cache
 
+# Subindo os containers com docker-compose
 echo "Subindo os containers com docker-compose..."
-docker-compose up --build --force-recreate --remove-orphans
+docker-compose up --build --force-recreate --remove-orphans -d
 
+# Exibindo logs do serviço Spring Boot em background
 echo "Exibindo logs do serviço Spring Boot..."
 docker-compose logs -f spring-app
+
+# O comando de logs está rodando em background agora, você pode continuar usando o terminal.
+# Se você quiser parar os logs, use `fg` para trazê-los de volta para o primeiro plano ou pressione Ctrl+C para matar o processo de logs
