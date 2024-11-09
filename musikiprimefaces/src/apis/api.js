3c366245ac9e416f521d02c3b420c752d508d5c4
Leonardo <leonardo.teixeira@osm.com.br>
2024-11-09 02:06:51 -0300

    Ajuste BASE_URL para dns publico aws

--- musikiprimefaces/src/apis/api.js
+++ musikiprimefaces/src/apis/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Ajuste a URL para a porta interna 8080, onde o Spring está rodando
-const BASE_URL = "http://spring-app:8081/api";
+const BASE_URL = "http://ec2-52-14-103-239.us-east-2.compute.amazonaws.com:8081/api";
 
 export default axios.create({
     baseURL: BASE_URL,
