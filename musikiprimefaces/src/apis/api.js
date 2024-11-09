c2324da02643ab58808fa03ccb3f5c3981e450b2
Leonardo <leonardo.teixeira@osm.com.br>
2024-11-09 01:48:36 -0300

    Adição BASE_URL spring-app:8081

--- musikiprimefaces/src/apis/api.js
+++ musikiprimefaces/src/apis/api.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
-//const BASE_URL = "https://musiki-api.onrender.com:8082/api"
 
-const BASE_URL = "http://52.14.103.239:8081/api"
+// Ajuste a URL para a porta interna 8080, onde o Spring está rodando
+const BASE_URL = "http://spring-app:8081/api";
 
 export default axios.create({
-    baseURL:BASE_URL,
-    mode:'cors',
+    baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json',
         'Accept' : 'application/json'
