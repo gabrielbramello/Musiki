9ac6ed604129916628c1f6b16e676a4c8aae3ccd
Leonardo <leonardo.teixeira@osm.com.br>
2024-11-11 22:01:55 -0300

    Ajuste dns maquina aws

--- musikiprimefaces/src/apis/api.js
+++ musikiprimefaces/src/apis/api.js
@@ -1,12 +1,22 @@
 import axios from 'axios';
 
-// Ajuste a URL para a porta interna 8080, onde o Spring está rodando
-const BASE_URL = "http://ec2-13-58-38-21.us-east-2.compute.amazonaws.com:8081/api";
+//const API_URL = process.env.REACT_APP_API_URL;
+//const PORTA_REACT = process.env.REACT_APP_PORTA;
+
+//console.log('REACT_APP_API_URL:', API_URL); // Deve imprimir "localhost"
+//console.log('REACT_APP_API_URL:', PORTA_REACT); // Deve imprimir "8081"
+
+//const BASE_URL = `http://${API_URL}:${PORTA_REACT}/api`;
+
+const BASE_URL = `http://ec2-3-147-126-64.us-east-2.compute.amazonaws.com:8081/api`;
+
+console.log('BASE_URL:', BASE_URL); // Deve imprimir "8081"
+
 
 export default axios.create({
     baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json',
-        'Accept' : 'application/json'
+        'Accept': 'application/json'
     }
 });
