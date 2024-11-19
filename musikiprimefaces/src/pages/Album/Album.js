109998dcb4e32b5b1e3e8fcafbfad0fc8fa8ff60
Leonardo <leonardo.teixeira@osm.com.br>
2024-11-18 21:56:42 -0300

    Substituição: mensagem api carregando -> componente skelleton - (album/artista)

--- musikiprimefaces/src/pages/Album/Album.js
+++ musikiprimefaces/src/pages/Album/Album.js
@@ -10,6 +10,7 @@ import CardWithList from '../../components/cards/CardWithList';
 import Header from '../../components/menu/Header';
 import { Link } from 'react-router-dom';
 import { Button } from 'primereact/button';
+import { Skeleton } from 'primereact/skeleton'; 
 import { decodeToken } from '../../utils/Utils';
 
 const Album = () => {
@@ -91,7 +92,23 @@ const Album = () => {
 
 
 	if (isLoading) {
-		return <p>Api do Spotify Carregando...</p>;
+		return (
+			<div className="border-round border-1 surface-border p-4 surface-card">
+				<div className="flex mb-3">
+					<Skeleton shape="circle" size="4rem" className="mr-2"></Skeleton>
+					<div>
+						<Skeleton width="10rem" className="mb-2"></Skeleton>
+						<Skeleton width="5rem" className="mb-2"></Skeleton>
+						<Skeleton height=".5rem"></Skeleton>
+					</div>
+				</div>
+				<Skeleton width="100%" height="150px"></Skeleton>
+				<div className="flex justify-content-between mt-3">
+					<Skeleton width="4rem" height="2rem"></Skeleton>
+					<Skeleton width="4rem" height="2rem"></Skeleton>
+				</div>
+			</div>
+		)
 	}
 	console.log(data)
 
